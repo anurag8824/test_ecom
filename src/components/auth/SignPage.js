f5015deb2cf49e789b9a4a33dd241150d3a1e2b2
rahulk67 <rahulkumawat6767@gmail.com>
2024-09-21 15:16:48 +0530

    mesg

--- src/components/auth/SignPage.js
+++ src/components/auth/SignPage.js
@@ -14,6 +14,10 @@ const SignPage = () => {
 
     const navigate = useNavigate();
 
+    const checking = Email.length > 0
+    if(checking) {
+        
+
 
 
     const showForm = (event) => {
@@ -50,6 +54,7 @@ const SignPage = () => {
                 console.log(err);
             })
     }
+}
 
 
 
