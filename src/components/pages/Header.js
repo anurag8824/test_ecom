d1b6d195edc7ff16bfe3128a0db21a96970ec459
rahulk67 <rahulkumawat6767@gmail.com>
2024-09-23 10:43:51 +0530

    first

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState , useEffect } from 'react';
+import axios from 'axios';
 
 const Navbar = () => {
 
@@ -22,7 +22,7 @@ const Navbar = () => {
     
   }, )
 
-  
+
   
 
 
